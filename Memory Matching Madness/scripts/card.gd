b8083e655f100f75a6deb5754b2edbc42ce07f60
BeesOffRice <ddemetria1204@gmail.com>
2023-10-19 14:59:55 -0400

    moved code files to the correct folder

--- Memory Matching Madness/scripts/card.gd
+++ Memory Matching Madness/scripts/card.gd
@@ -1,19 +1,36 @@
 extends Area2D
+signal CardClicked
 
-var stateHandler
-
-
+var isFlipped: bool = false
+@export var cardValue: Color  
+var cardBack: Color = Color(Color.BLACK);
+var isMatched: bool = false;
+var canFlip: bool = true;
 # Called when the node enters the scene tree for the first time.
 func _ready():
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+func flipDown():
+	$StateMachine.changeState("carddown")
 
+func setFlipped(val):
+	isFlipped=val
+	
+func setClickable(val):
+	canFlip = val
 
-func _on_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			stateHandler.flipCard()
-			
+func getisMatched():
+	return isMatched
+
+func setPos(pos,col):
+	position=pos
+	cardValue=col
+
+#sends the card and the number 1 to be handled by the game scene
+func _on_card_down_card_clicked(num):
+	if canFlip:
+		CardClicked.emit(num,self)
